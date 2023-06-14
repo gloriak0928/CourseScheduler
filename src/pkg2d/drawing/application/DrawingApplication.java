d2cf593e6e5ca21a9a72259c67712b27ca590c4f
Gloriakyx <gloriak0928@gmail.com>
2023-06-14 14:17:07 +0800

    Finished Version

---
+++ src/pkg2d/drawing/application/DrawingApplication.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg2d.drawing.application;
+import java.awt.Font;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+/**
+ *
+ * @author mac
+ */
+public class DrawingApplication extends JPanel{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
